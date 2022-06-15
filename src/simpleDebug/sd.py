67176fff9a7b044277517abb8084ea589d7d9e8c
Gautham Venkateshwaran <gauthamv6002@gmail.com>
2022-06-14 23:14:43 -0600

    Finished sd.p, fr this time

--- src/simpleDebug/sd.py
+++ src/simpleDebug/sd.py
@@ -1,10 +1,13 @@
 from inspect import getframeinfo, stack
 from termcolor import colored
 import os
+import time
+import math
 
 
 class SimpleDebug:
     def __init__(self, **kwargs):
+        self.startTime = time.perf_counter()
         self.statusColors = kwargs.get("statusColors", {
             "OFF": "magenta",
             "O": "magenta",
@@ -22,7 +25,15 @@ class SimpleDebug:
             "T": "cyan",
         })
 
+    def getTimestamp(self, t):
+        mins = math.floor((t - self.startTime)/60)
+        secs = (t - self.startTime) - mins*60
+        return f"{mins}:{secs}"
+
     def simpleAutoPrint(self, **kwargs):
+        timestamp = colored(self.getTimestamp(
+            time.perf_counter()), "grey", "on_white", attrs=["dark"])
+
         status = kwargs.get("status", "DEBUG")
         if kwargs["color"] != None:
             status_clr = kwargs["color"]
@@ -34,20 +45,23 @@ class SimpleDebug:
         filePath = os.path.basename(
             caller.filename) if not kwargs["showFullPath"] else caller.filename
         line = colored(f"Ln {caller.lineno}", "white",
-                       f"on_{status_clr}", attrs=['bold'])
+                       f"on_{status_clr}")
         file = colored(filePath, attrs=['underline'])
-        root_function = "exec()"
 
         msg = kwargs.get("msg", "")
-        print(
-            f'{status}\t|SAP CALL|\t{line}, in {file} at {root_function}\t{msg}')
+        if msg != "":
+            msg = "Message: " + \
+                colored(kwargs.get("msg", ""), "grey", attrs=['bold'])
 
-    def p(self, msg="", status="DEBUG", color=None, showFullPath=False, **kwargs):
+        print(f'{timestamp}    {status}    |SAP CALL|    {line}, in {file}\t{msg}')
+
+    def p(self, status="DEBUG", msg="", color=None, showFullPath=False, **kwargs):
         kwargs["status"] = status
         kwargs["showFullPath"] = showFullPath
         kwargs["color"] = color
+        kwargs["msg"] = msg
         self.simpleAutoPrint(**kwargs)
 
 
 sd = SimpleDebug()
-sd.p("bruh", "TRACE")
+sd.p()

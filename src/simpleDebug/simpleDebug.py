165ff79d954eb2056dc307a177f42c5c8d47ab4e
Gautham Venkateshwaran <gauthamv6002@gmail.com>
2022-06-14 21:48:07 -0600

    Added functionality

--- src/simpleDebug/simpleDebug.py
+++ src/simpleDebug/simpleDebug.py
@@ -1,27 +1,40 @@
+from inspect import getframeinfo, stack
 import traceback
-# from termcolor import colored
+from colorama import Fore, Style
 
-# SAP Call Format:
-# *STATUS*  |SAP CALL|   Ln *LINE*, Col *Column* in *FILE*  at  *ROOT_FUNCTION*   *INFO_OPT*
-
-# ie.,
-# ERR_CRITICAL  |SAP CALL|  Ln 28, Col 23 in main.py  at run()    "a function lol"
 
 class SimpleDebug:
-    def __init__(self):
-        pass
+    def __init__(self, **kwargs):
+        self.statusColors = kwargs.get("statusColors", {
+            "OFF": "magenta",
+            "O": "magenta",
+            "ERROR": "red",
+            "ERR": "red",
+            "E": "red",
+            "WARNING": "yellow",
+            "WARN": "yellow",
+            "W": "yellow",
+            "DEBUG": "green",
+            "D": "green",
+            "INFO": "blue",
+            "I": "blue",
+            "TRACE": "cyan",
+            "T": "cyan",
+        })
 
     def simpleAutoPrint(self, **kwargs):
         status = kwargs.get("status", "LOG")
         msg = kwargs.get("msg", "")
-        line = 23
-        col = 12
-        file = "main.py"
-        root_function = "run()"
-        print(f'{status}\t|SAP CALL|\tLn {line}, col {col} in {file}. At {root_function}\t{msg}')
+        caller = getframeinfo(stack()[-1][0])
+        line = caller.lineno
+        file = caller.filename
+        root_function = "exec()"
+        print(f'{status}\t|SAP CALL|\tLn {line} in {file} at {root_function}\t{msg}')
 
-    def sap(self, **kwargs):
+    def sap(self, status="LOG", **kwargs):
+        kwargs.status = status
         self.simpleAutoPrint(**kwargs)
+
 
 sd = SimpleDebug()
 sd.sap()
